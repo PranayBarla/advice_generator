19c6f595185ed69f53e95d43466251184f39fb92
Pranay Barla <Pranaybarla66@gmail.com>
2024-02-15 18:47:05 +0530

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App (){
+  
+const[advice,setAdvice] = useState("");
+const[count, setCount] = useState(0);
+
+
+  async function getAdvice (){
+
+    const findData = await fetch('https://api.adviceslip.com/advice');
+    const data = await findData.json();
+    // console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount((c) => c+1); //Number of times button clicked
+  }
+  useEffect(function(){getAdvice();},[]); //generates "Advices" just after reloading
+  
+    return (
+      <div>
+        <h1>{advice}</h1>
+        <button onClick={getAdvice}>get Advice</button>
+        <Message count={count}/>
+
+      </div>
+    );
+
+    function Message(prop){
+      return (
+        <p>You got <strong>{prop.count}</strong> Advices !!</p>
+      );
+    }
+  
 }
 
-export default App;
